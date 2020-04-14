685a102024c49f617fb4d0a6bfe029c659ae035d
Aya Nashawati <61278736+aya-nashawati@users.noreply.github.com>
2020-04-14 17:01:51 +0300

    Add files via upload

---
+++ LF-Version4/SingletonLogger/Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SingletonLogger
+{
+    public class Logger
+    {
+        private Logger() { }
+
+        private string fmt = "dd/mm/yy hh:mm";
+
+        private static Logger _logger;
+        public static Logger GetLogger()
+        {
+            if(_logger==null)
+            {
+                _logger = new Logger();
+            }
+            return _logger;
+        }
+        public void Log(string msg) => Console.WriteLine($"{DateTime.Now} {msg}");
+        public void ChangeLogformat(string fmt)
+        {
+            this.fmt = fmt;
+        }
+        public override string ToString() => fmt;
+    }
+}
